a14bcaad2f0b150ebb8eb24e2322a3b981192441
Michael Hochleitner <hochleitner@protonmail.com>
2019-11-23 15:20:24 +0100

    Adds count of comparisons

--- binary_search.jl
+++ binary_search.jl
@@ -1,20 +1,21 @@
-function binary_search(list, item)
+function binary_search(list, item, comparisons_inner=0)
     low = 1
     high = length(list)
 
     while low <= high
         mid = Int(floor((low + high)/2))
         guess = list[mid]
+        comparisons_inner+=1
         if guess == item
-            return mid
+            return mid,comparisons_inner
         elseif guess > item
             high = mid - 1
         else
             low = mid + 1
         end
     end
-    return 0
+    return 0,comparisons_inner
 end
 
-my_list = [1, 3, 5, 7, 9]
-println(binary_search(my_list, 10))
+my_list = [5, 8, 11, 15, 21, 23, 100, 223]
+println(binary_search(my_list, 223))
