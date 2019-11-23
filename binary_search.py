69391f8c4e3ec198c4537af1800fbf833da7207e
Michael Hochleitner <hochleitner@protonmail.com>
2019-11-23 12:09:10 +0100

    Returns steps_inner when the given element is not in the array
    This enables counting steps and searching for elements which are not in the array.

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,20 @@
-def binary_search(a_list, item):
+def binary_search(a_list, item, steps_inner=0):
     low = 0
     high = len(a_list) - 1
 
     while low <= high:
         mid = (low + high) / 2
         guess = a_list[mid]
+        steps_inner += 1
         if guess == item:
-            return mid
+            return mid, steps_inner
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
-    return None
+    return None, steps_inner
 
 
 my_list = [5, 8, 11, 15, 21, 23, 100, 223]
-index= binary_search(my_list, 224)
-print(index)
+index, steps = binary_search(my_list, 224)
+print(index, steps)
