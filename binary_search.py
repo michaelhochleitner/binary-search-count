85e8908f3788cce7d74e22d9ad447fcd4e97373a
Michael Hochleitner <hochleitner@protonmail.com>
2019-11-23 12:38:57 +0100

    Refactor variable name

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,20 @@
-def binary_search(a_list, item, steps_inner=0):
+def binary_search(a_list, item, comparisons_inner=0):
     low = 0
     high = len(a_list) - 1
 
     while low <= high:
         mid = (low + high) / 2
         guess = a_list[mid]
-        steps_inner += 1
+        comparisons_inner += 1
         if guess == item:
-            return mid, steps_inner
+            return mid, comparisons_inner
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
-    return None, steps_inner
+    return None, comparisons_inner
 
 
 my_list = [5, 8, 11, 15, 21, 23, 100, 223]
-index, steps = binary_search(my_list, 224)
-print(index, steps)
+index, comparisons = binary_search(my_list, 223)
+print(index, comparisons)
